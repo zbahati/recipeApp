6c69367889fa71874918aeb791b5ed02dd477873
joyce <joyce.ann.cruz@gmail.com>
2023-11-14 20:05:26 +0800

    Create view for Food list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # Users
-User.create(username: 'user1')
-User.create(username: 'user2')
+User.create(name: 'user1')
+User.create(name: 'user2')
 
 # Foods
-Food.create(name: 'Food 1', measurement_unit: 'grams', price: 10.99, quantity: 500, user_id: 1)
-Food.create(name: 'Food 2', measurement_unit: 'pieces', price: 2.5, quantity: 10, user_id: 2)
+Food.create(name: 'Food 1', measurementUnit: 'grams', price: 10.99, quantity: 500, user_id: 1)
+Food.create(name: 'Food 2', measurementUnit: 'pieces', price: 2.5, quantity: 10, user_id: 2)
+Food.create(name: 'Food 3', measurementUnit: 'pieces', price: 25, quantity: 1, user_id: 2)
+Food.create(name: 'Food 4', measurementUnit: 'liters', price: 7.0, quantity: 5, user_id: 2)
