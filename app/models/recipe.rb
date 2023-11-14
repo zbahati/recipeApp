a4cc71b0661da5cac434449604bcbee55e337d8e
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-14 11:18:41 +0200

    Ft navigation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,22 +3,6 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, :preparation_time, :cooking_time, presence: true
-
-  #custom validations
-  validate :validate_time_format
-
-  private
-
-  def validate_time_format
-    validate_time_format_for(:preparation_time)
-    validate_time_format_for(:cooking_time)
-  end
-
-  def validate_time_format_for(added_time)
-    value = send(added_time)
-    return if value.blank?
-    unless value.match?(/\A\d+(\.\d+)? (hours|minutes)\z/i)
-      errors.add(added_time, "should be number + hours(if many) or hour(if one) or munites")
-    end
-  end
+  validates :description, length: {minimum:10, maximum:1000}
+  validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_175028) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_083010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_175028) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.text "preparation_time"
-    t.text "cooking_time"
+    t.decimal "preparation_time"
+    t.decimal "cooking_time"
     t.text "description"
     t.boolean "public", default: false
     t.bigint "user_id", null: false
