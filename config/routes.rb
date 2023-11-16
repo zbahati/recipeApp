03699a9aee2002d0b08ee596bf67fbbe8d60a6fb
joyce <joyce.ann.cruz@gmail.com>
2023-11-17 05:09:38 +0800

    Removed duplicate route for general_shopping_list

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :users
   resources :general_shopping_list, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
